c58b717647f53a002446433d52b22343ff1947e5
Deborah D <deborah.douillet@viacesi.fr>
2022-12-27 21:35:42 +0100

    clean form to add function, but 2 error microsoft

--- WinFormsApp1/AdminEditor.cs
+++ WinFormsApp1/AdminEditor.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -51,6 +52,8 @@ namespace WinFormsApp1
             dataGridViewEmployeeEdit.Columns[3].HeaderText = "Téléphone Fixe";
             dataGridViewEmployeeEdit.Columns[4].HeaderText = "Mobile";
             dataGridViewEmployeeEdit.Columns[5].HeaderText = "Email";
+            dataGridViewEmployeeEdit.Columns[6].HeaderText = "Site";
+            dataGridViewEmployeeEdit.Columns[7].HeaderText = "Service";
         }
 
         private void AdminEditor_Load_1(object sender, EventArgs e)
@@ -62,8 +65,39 @@ namespace WinFormsApp1
 
         private void add_employee_Click(object sender, EventArgs e)
         {
-            // add a new item to the database
-            Employee employee = new Employee
+           //Regex numberRegex = new Regex("^[0-9]+$");
+           // String fixNumber = txt_employee_landline.Text;
+            //int countNumber = fixNumber.Split().Length + 1;
+           // String mobileNumber = txt_employee_mobile.Text;
+
+            //Console.WrteLine(coun);
+
+          //  if (!numberRegex.IsMatch(fixNumber) && !numberRegex.IsMatch(mobileNumber))
+               //  {
+           //     MessageBox.Show("Le numéro de téléphone fixe doit comporter uniqument des chiffres");
+          //  }
+           // else if (countNumber != 10)
+            //{
+              //  MessageBox.Show("Le numéro ne comporte pas 10 chiffres");
+            //}
+           // else 
+            if (
+                String.IsNullOrEmpty(txt_employee_lastname.Text) ||
+                String.IsNullOrEmpty(txt_employee_firstname.Text) ||
+                String.IsNullOrEmpty(txt_employee_landline.Text) ||
+                String.IsNullOrEmpty(txt_employee_mobile.Text) ||
+                String.IsNullOrEmpty(txt_employee_email.Text) ||
+                String.IsNullOrEmpty(txt_employee_site.Text) ||
+                String.IsNullOrEmpty(txt_employee_service.Text)
+                )
+            {
+                MessageBox.Show("Erreur : au moins un champ est vide");
+            }
+            else
+            {
+
+                // add a new item to the database
+                Employee employee = new Employee
             {
                 lastname = txt_employee_lastname.Text,
                 firstname = txt_employee_firstname.Text,
@@ -89,12 +123,20 @@ namespace WinFormsApp1
             txt_employee_service.Text = String.Empty;
 
             MessageBox.Show("L'employé(e) " + employee.lastname + " " + employee.firstname+ " a été ajouté");
+            }
         }
 
         private void addService_Click(object sender, EventArgs e)
         {
-            // add a new item to the database
-            Department department = new Department
+
+            if(String.IsNullOrEmpty(txt_department_name.Text))
+            {
+                MessageBox.Show("Erreur : Le champ est vide");
+            } 
+            else
+            {
+                // add a new item to the database
+                Department department = new Department
             {
                 name = txt_department_name.Text,
             };
@@ -108,12 +150,18 @@ namespace WinFormsApp1
             txt_department_name.Text = String.Empty;
 
             MessageBox.Show("Le service " + department.name + " a été ajouté");
-           
+            }
         }
 
         private void addSite_Click(object sender, EventArgs e)
         {
-            Site site = new Site
+            if (String.IsNullOrEmpty(txt_site_name.Text))
+            {
+                MessageBox.Show("Erreur : Le champ est vide");
+            } 
+            else
+            {
+                Site site = new Site
             {
                 name = txt_site_name.Text,
             };
@@ -127,6 +175,7 @@ namespace WinFormsApp1
             txt_site_name.Text = String.Empty;
 
             MessageBox.Show("Le site " + site.name + " a été ajouté");
+            }
         }
 
         private void label10_Click(object sender, EventArgs e)
